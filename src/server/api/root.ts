4c36b0bb550e158a4e7cec902a9c866f7e737c1d
Blair McAlpine <blair.mcalpine77@gmail.com>
2024-01-15 17:46:31 -0500

    Update comments

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,6 @@
 import { puzzleRouter } from "@/server/api/routers/puzzle";
 import { createTRPCRouter } from "@/server/api/trpc";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
   puzzle: puzzleRouter,
 });
