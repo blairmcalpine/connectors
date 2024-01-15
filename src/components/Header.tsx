bb01795cc3bb78f749ea2a280a1ea56f1bcd6f09
Blair McAlpine <blair.mcalpine77@gmail.com>
2024-01-15 17:57:40 -0500

    Lower header font size on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,18 +10,20 @@ type HeaderProps = {
 
 export const Header = ({ title, timer }: HeaderProps) => {
   return (
-    <header className="flex flex-col items-center gap-4 border-b py-8 md:flex-row md:items-end md:px-24 md:py-16">
-      <div className="flex items-end gap-4">
+    <header className="flex flex-col items-center gap-4 border-b py-4 md:flex-row md:items-end md:px-24 md:py-16">
+      <div className="flex items-center gap-3">
         <Link href="/">
           <Image
             src="/connectors.png"
             alt="Connectors"
             width={48}
             height={48}
-            className="h-12 w-12"
+            className="w-10 md:w-12"
           />
         </Link>
-        <h1 className="h-12 text-center text-5xl font-bold">{title}</h1>
+        <h1 className="h-10 truncate text-center text-4xl font-bold md:h-12 md:text-5xl">
+          {title}
+        </h1>
       </div>
       <h2 className="hidden text-3xl font-light md:block">
         {format(new Date(), "MMMM d, yyyy")}
